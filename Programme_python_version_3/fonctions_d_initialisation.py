ad1dcb5fc732df851f8dc8850ad93c3f649db6e4
Brandon CASTEL <brandoncastel2@gmail.com>
2020-08-25 22:50:24 +0200

    Remove isis project files

--- Programme_python_version_3/fonctions_d_initialisation.py
+++
@@ -1,7 +0,0 @@
-import time, test
-import fonctions_de_fichier as fdf
-
-
-
-
-
--- Programme_python_version_4/testMain.py
+++
@@ -1,9 +0,0 @@
-import test
-import sys
-
-def main():
-    test.saisieCoordonnes()
-    #test.connection()
-
-if __name__ == "__main__":
-    main()
